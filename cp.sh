78b9e0112656d02afc870614117afac5dfac3bea
yli14 <975709941@qq.com>
2018-01-08 11:48:22 +0800

    try

---
+++ cp.sh
@@ -0,0 +1,31 @@
+#echo "hello" >> /folk/yli14/kk.log
+#rm -rf /folk/yli14/testc.png
+#get the newest cov pic to /folk/yli14/testc.png
+#python /folk/yli14/test.py
+:<<!
+echo "123456" | sudo -S cp /folk/yli14/coverity.png /usr/local/nginx/html/test_report
+echo "123456" | sudo -S cp /folk/yli14/coverity.pdf /usr/local/nginx/html/test_report
+#rm -rf /folk/yli14/testc.png
+#echo "hello2" >> /folk/yli14/kk.log
+
+#get the nightly json file to this dir
+# userinfo.txt contain the pie chart parameters and nightly cfg infos.
+echo "123456" | sudo -S rm -rf /usr/local/nginx/html/test_report/userinfo.txt
+echo "123456" | sudo -S cp /folk/yli14/userinfo.txt /usr/local/nginx/html/test_report
+echo "123456" | sudo -S cp /folk/yli14/nightly.json /usr/local/nginx/html/test_report
+#echo "123456" | sudo -S cp /folk/yli14/test/covNewestInfo.txt /usr/local/nginx/html/test_report
+
+#echo "123456" | sudo -S cp /folk/yli14/test/nightly.ini /usr/local/nginx/html/test_report
+echo "123456" | sudo -S cp /folk/yli14/nightlyTrend.png /usr/local/nginx/html/test_report
+echo "123456" | sudo -S cp /folk/vxMW/nightly/wassp-repos/testcases/vxworks7/script/ci/defect.html /usr/local/nginx/html/test_report
+#echo "123456" | sudo -S cp /folk/yli14/emailmessage.html /usr/local/nginx/html/test_report
+echo "123456" | sudo -S cp /folk/yli14/df.html /usr/local/nginx/html/test_report
+echo "123456" | sudo -S cp /folk/yli14/emailmessage.html /usr/local/nginx/html/test_report
+!
+echo "123456" | sudo -S cp /folk/yli14/test/runOrNot2.json /usr/local/nginx/html/nightlyReport/buildInfoT.json
+#echo "123456" | sudo -S cp /folk/yli14/df.html /usr/local/nginx/html/nightlyReport
+echo "123456" | sudo -S cp /folk/yli14/nightlyTrend.png /usr/local/nginx/html/nightlyReport
+
+
+
+#echo "123456" | sudo -S cat spindata.json | sed 's:\\\/:\/:g' > spinData.json
